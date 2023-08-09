f3dd94af01127202964a00ef2e7f7c1b0972f149
siddharth mishra <smsiddharthmishra@gmail.com>
2023-08-10 07:57:15 +1000

    UI changes

--- ecolife/frontend/src/pages/Aboutus.jsx
+++ ecolife/frontend/src/pages/Aboutus.jsx
@@ -3,34 +3,36 @@ import styles from "./Aboutus.module.css";
 
 export default function Aboutus() {
   return (
-    <main className={styles.aboutus}>
+    <div style={{ height: "100vh" }}>
       <PageNav />
-      <div></div>
-      <section>
-        <h1>
-          Welcome to our Ecolife!
-          <br />
-        </h1>
-        <p>
-          At Ecolife, we are dedicated to promoting sustainability and
-          empowering individuals to take meaningful actions towards reducing
-          their carbon footprint. Our Carbon Footprint Calculator is designed to
-          provide you with valuable insights into your environmental impact,
-          helping you make informed decisions and contribute to a greener
-          future.
-        </p>
-        <h2>
-          Our Mission:
-          <br />
-        </h2>
+      <div className={styles.aboutus}>
+        <div></div>
+        <section className={styles["aboutus-section"]}>
+          <h1>
+            Welcome to our Ecolife!
+            <br />
+          </h1>
+          <p>
+            At Ecolife, we are dedicated to promoting sustainability and
+            empowering individuals to take meaningful actions towards reducing
+            their carbon footprint. Our Carbon Footprint Calculator is designed
+            to provide you with valuable insights into your environmental
+            impact, helping you make informed decisions and contribute to a
+            greener future.
+          </p>
+          <h2>
+            Our Mission:
+            <br />
+          </h2>
 
-        <p>
-          At Ecolife, our mission is to raise awareness about the importance of
-          environmental responsibility and inspire positive change. We believe
-          that every small action counts and that together, we can make a
-          significant impact on the health of our planet.
-        </p>
-      </section>
-    </main>
+          <p>
+            At Ecolife, our mission is to raise awareness about the importance
+            of environmental responsibility and inspire positive change. We
+            believe that every small action counts and that together, we can
+            make a significant impact on the health of our planet.
+          </p>
+        </section>
+      </div>
+    </div>
   );
 }
--- ecolife/frontend/src/pages/Home.jsx
+++ ecolife/frontend/src/pages/Home.jsx
@@ -1,18 +1,26 @@
 import PageNav from "../components/PageNav";
-import styles from "./Home.module.css";
-
+import styles from "./Home.module.scss";
+import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
-    <main className={styles.homepage}>
+    <main className={styles.main}>
       <PageNav />
-      <section>
+      <div className={styles.stage}>
+        <div className={styles.actor}></div>
+        <div className={styles.actor}></div>
+        <div className={styles.actor}></div>
+      </div>
+      <div className={styles["article-home"]}>
         <h1>
-          Save electricity save earth.
-          <br />
-          Ecolife keeps track of your carbon footprint.
+          Empower Your Choices: Measure, Compare, and Reduce Your Carbon
+          Footprint!
         </h1>
-        <h2></h2>
-      </section>
+      </div>
+      <div className={styles["getstarted"]}>
+        <div className={styles["btn-home"]}>
+          <NavLink to="/usage">Get Started</NavLink>
+        </div>
+      </div>
     </main>
   );
 }
