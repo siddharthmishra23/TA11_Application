8795742204fd28a905637239d3239f2b3742daf1
siddharth mishra <smsiddharthmishra@gmail.com>
2023-08-10 00:18:11 +1000

    deployment done

--- ecolife/backend/routes/Electricity.js
+++ ecolife/backend/routes/Electricity.js
@@ -4,22 +4,22 @@ const dbConfig = require("../config/dbConfig");
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   ssl: { ca: fs.readFileSync(process.env.CA_CERT_PATH) },
-// });
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  //ssl: { ca: fs.readFileSync(process.env.CA_CERT_PATH, "utf-8") },
 });
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
 
 router.get("/", async (req, res) => {
   try {
